1665e21f8dc1baf1fcd48f7042bb85c7c1e13a0d
Seungwon Oh <ohsing0906@gmail.com>
2018-02-01 15:25:17 +0900

    Initial Commit

---
+++ project_test/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  project_test
+//
+//  Created by Seungwon Oh on 2018. 2. 1..
+//  Copyright © 2018년 Seungwon Oh. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
